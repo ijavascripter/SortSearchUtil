7987002ef8c4dd3849760bfce4275ba017495e20
journeyjt <KFKn7aAJvUuGS830CY8z>
2016-11-26 17:18:55 -0800

    Added a tester class.

--- src/SortSearchUtil.java
+++ src/SortSearchUtil.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 /**
  * Created by jeff on 10/29/15.
+ *
+ * A Collection of search and sort algorithms
  */
 public class SortSearchUtil {
 
@@ -140,64 +142,5 @@ public class SortSearchUtil {
 
         if (high > i)
             quickSort(arr, i, high);
-    }
-}
-
-class SortSearchUtilTester{
-    public static class randArray {
-        private Random random;
-        private ArrayList<Integer> intArray;
-
-        public randArray(int arraySize) {
-            this.random = new Random(System.currentTimeMillis());
-            this.intArray = new ArrayList<>();
-            for (int i = 0; i < arraySize; i++) {
-                this.intArray.add(i, random.nextInt());
-            }
-        }
-
-        public ArrayList<Integer> getIntArray() {
-            return this.intArray;
-        }
-    }
-
-    public static void main(String[] args){
-        SortSearchUtil ssu = new SortSearchUtil();
-        randArray randomArray = new randArray(10000000);
-        ArrayList<Integer> test = randomArray.getIntArray();
-        //ssu.shellSort(test);
-        //printArray(test);
-        test = randomArray.getIntArray();
-        ArrayList<Integer> test2 = test;
-        ArrayList<Integer> test3 = test;
-        //ssu.quickSort(test, 0, test.size() -1);
-
-        long startTime = System.nanoTime();
-        long endTime = System.nanoTime();
-        ssu.mergeSort(test);
-        long duration = (endTime - startTime);
-        System.out.println("Merge Sort: " + duration);
-
-
-        startTime = System.nanoTime();
-        endTime = System.nanoTime();
-        ssu.quickSort(test2, 0, test.size() - 1);
-        duration = (endTime - startTime);
-        System.out.println("Quick Sort: " + duration);
-
-
-        startTime = System.nanoTime();
-        endTime = System.nanoTime();
-        ssu.shellSort(test3);
-        duration = (endTime - startTime);
-        System.out.println("Shell Sort: " + duration);
-
-        //System.out.println(ssu.binarySearch(5,test));
-    }
-
-    private static void printArray(int[] array){
-        for(int i = 0; i < array.length; i++){
-            System.out.println(array[i]+"");
-        }
     }
 }
---
+++ src/Tester.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by journ on 11/26/2016.
+ */
+public class Tester {
+    public static class randArray {
+        private Random random;
+        private ArrayList<Integer> intArray;
+
+        public randArray(int arraySize) {
+            this.random = new Random(System.currentTimeMillis());
+            this.intArray = new ArrayList<>();
+            for (int i = 0; i < arraySize; i++) {
+                this.intArray.add(i, random.nextInt());
+            }
+        }
+
+        public ArrayList<Integer> getIntArray() {
+            return this.intArray;
+        }
+    }
+
+    public static void main(String[] args){
+        SortSearchUtil ssu = new SortSearchUtil();
+        randArray randomArray = new randArray(10000000);
+        ArrayList<Integer> test = randomArray.getIntArray();
+        //ssu.shellSort(test);
+        //printArray(test);
+        test = randomArray.getIntArray();
+        ArrayList<Integer> test2 = test;
+        ArrayList<Integer> test3 = test;
+        //ssu.quickSort(test, 0, test.size() -1);
+
+        long startTime = System.nanoTime();
+        long endTime = System.nanoTime();
+        ssu.mergeSort(test);
+        long duration = (endTime - startTime);
+        System.out.println("Merge Sort: " + duration);
+
+
+        startTime = System.nanoTime();
+        endTime = System.nanoTime();
+        ssu.quickSort(test2, 0, test.size() - 1);
+        duration = (endTime - startTime);
+        System.out.println("Quick Sort: " + duration);
+
+
+        startTime = System.nanoTime();
+        endTime = System.nanoTime();
+        ssu.shellSort(test3);
+        duration = (endTime - startTime);
+        System.out.println("Shell Sort: " + duration);
+
+        //System.out.println(ssu.binarySearch(5,test));
+    }
+
+    private static void printArray(int[] array){
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]+"");
+        }
+    }
+}
